5b6642d07028439a3e2bbb35225b2e99a1e35f3d
Ceri Thomas <ct229252@falmouth.ac.uk>
2019-11-18 16:55:17 +0000

    Repositioned the play and quit button and updated journal script.

--- The Beneath/Assets/Scripts/JournalScript.cs
+++ The Beneath/Assets/Scripts/JournalScript.cs
@@ -8,7 +8,8 @@ public class JournalScript : MonoBehaviour
     public CrabInteraction()
     {
         //if crab is clicked on and successfully interrogated then crab page overlays blank page
-        //Crab overlay over half of BGV3
+        //Crab overlay covers half of BGV3
+        Debug.Log("Crab");
 
 
     }
@@ -16,36 +17,36 @@ public class JournalScript : MonoBehaviour
     //Fish
     public FishInteraction()
     {
-
+        Debug.Log("Fish");
     }
     //Jellyfish
     public JellyfishInteraction()
     {
-
+        Debug.Log("Jellyfish");
     }
     //MorayEel
     public MorayInteraction()
     {
-
+        Debug.Log("Moray");
     }
     //Seahorse
     public SeahorseInteraction()
     {
-
+        Debug.Log("Seahorse");
     }
     //Seaweed
     public SeaweedInteraction()
     {
-
+        Debug.Log("Seaweed");
     }
     //Shark
     public SharkInteraction()
     {
-
+        Debug.Log("Shark");
     }
     //Squid
     public SquidInteraction()
     {
-
+        Debug.Log("Squid");
     }
 }
