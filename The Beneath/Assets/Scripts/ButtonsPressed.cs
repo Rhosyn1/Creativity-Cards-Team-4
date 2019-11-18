ac73147d85c834ee6ff245aae78830952f486665
rhosyn1 <Orll@ndome2>
2019-11-18 10:04:05 +0000

    Added a Script file and made a script for pressing the buttons on start screen.

---
+++ The Beneath/Assets/Scripts/ButtonsPressed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayGame : MonoBehaviour
+{
+    public void PlayButtonPressed()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitButtonPressed()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
