eec310ed71f40b09954d499de6001445523144f1
Quinny18 <56097996+Quinny18@users.noreply.github.com>
2019-11-19 23:26:51 +0000

    Added Player movement via NavMesh. Added Player Health. Added Enemy Naval Mines that harm the player when touched.

---
+++ The Beneath/Assets/Exploding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploding : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            StartCoroutine(Countdown());
+
+        }
+    }
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
+
+
