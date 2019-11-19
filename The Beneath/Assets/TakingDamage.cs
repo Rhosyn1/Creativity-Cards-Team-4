cf691a130d5ba8c361f93333a92dcac52ce3e8f8
Quinny <Callum_Quinn1@outlook.com>
2019-11-19 00:58:53 +0000

    Added Player movement via NavMesh. Added Player Health. Added Enemy Terrain that harms player.

---
+++ The Beneath/Assets/TakingDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakingDamage : MonoBehaviour
+{
+    public int health = 10;
+    public int DamageToPlayer = 1;
+    public bool invincible = false;
+    public GameObject Player;
+    
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Terrain")
+        {
+            health -= 1;
+            StartCoroutine("InvincibilityTime");
+        }
+    }
+    IEnumerator InvincibilityTime()
+    {
+        invincible = true;
+        yield return new WaitForSeconds(1);
+        invincible = false;
+    }
+}
