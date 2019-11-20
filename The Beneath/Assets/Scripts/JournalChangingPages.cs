f06440880a5cfe23793423ccaca45eeb13406ad7
rhosyn1 <Orll@ndome2>
2019-11-20 09:55:18 +0000

    Added overlays to journal and assigned them to the script.

--- The Beneath/Assets/Scripts/JournalChangingPages.cs
+++ The Beneath/Assets/Scripts/JournalChangingPages.cs
@@ -7,6 +7,4 @@ public class JournalChangingPages : MonoBehaviour
    
     //check which overlays are enabled and disable those overlays when switching pages.
     //Enable the next overlays when page is switched if interaction has been made. 
-
-
 }
