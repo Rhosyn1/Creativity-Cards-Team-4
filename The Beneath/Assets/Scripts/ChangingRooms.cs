3ada268428cc218f4f598e9aca0956caa0c7c970
rhosyn1 <Orll@ndome2>
2019-11-21 17:32:12 +0000

    Started the room changing script.

---
+++ The Beneath/Assets/Scripts/ChangingRooms.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingRooms : MonoBehaviour
+{
+    //if player touches door then player is transported to next room
+    public Collider playerObject;
+    public Collider colourCollider;
+
+    public GameObject player;
+    private MeshRenderer render;
+
+    public Color myColour = new Color(75f, 28f, 82f, 255f);
+    void OnCollisionEnter(Collider other)
+    {
+        if(render.material.color == myColour)
+        {
+            player.transform.position = new Vector3(-31.08f,3.151f,-3.63f);
+            
+        }
+    }
+}
